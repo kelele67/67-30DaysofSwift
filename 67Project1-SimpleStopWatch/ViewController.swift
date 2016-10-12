eb6c24e81f003a9c03d09234aeb1578d0b7f584d
kelele67 <kingand67@outlook.com>
2016-10-12 10:11:46 +0800

    Initial Commit

---
+++ 67Project1-SimpleStopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  67Project1-SimpleStopWatch
+//
+//  Created by 刘启 on 2016/10/12.
+//  Copyright © 2016年 刘启. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 67Project1-SimpleStopWatchTests/_7Project1_SimpleStopWatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  _7Project1_SimpleStopWatchTests.swift
+//  67Project1-SimpleStopWatchTests
+//
+//  Created by 刘启 on 2016/10/12.
+//  Copyright © 2016年 刘启. All rights reserved.
+//
+
+import XCTest
+@testable import _7Project1_SimpleStopWatch
+
+class _7Project1_SimpleStopWatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
